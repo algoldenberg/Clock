a8ca3940c330847307ed7b02b071d36c0464fb6c
algoldenberga <algoldenberga@gmail.com>
2023-02-07 17:38:10 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+    window.setInterval(function() {
+        var date = new Date();
+        var today = new Date();
+
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var seconds = date.getSeconds();
+
+        if(hours <10) hours = "0" + hours;
+        if(minutes <10) minutes = "0" + minutes;
+        if(seconds <10) seconds = "0" + seconds;
+        
+
+        var clock = hours + ":" + minutes + ":" + seconds;
+        document.getElementById("clock").innerHTML = clock;
+        
+        var now = today.toLocaleDateString('en-US');
+        document.getElementById("calendar").innerHTML = now;
+        
+    }
+    );
+}
+
